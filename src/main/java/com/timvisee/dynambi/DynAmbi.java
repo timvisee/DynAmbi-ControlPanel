21a4db1eec988f92f754fc98be9ed98670049dfd
Tim Visée <timvisee@gmail.com>
2016-05-02 20:30:30 +0200

    Add application name and version constants

--- src/main/java/com/timvisee/dynambi/DynAmbi.java
+++ src/main/java/com/timvisee/dynambi/DynAmbi.java
@@ -2,6 +2,30 @@ package com.timvisee.dynambi;
 
 public class DynAmbi {
 
+    /**
+     * Application name.
+     */
+    public static final String APP_NAME = "DynAmbi - Control Panel";
+
+    /**
+     * Application version name.
+     */
+    public static final String APP_VERSION_NAME = "0.1";
+
+    /**
+     * Application version code.
+     */
+    public static final int APP_VERSION_CODE = 1;
+
+    /**
+     * Get the full application name string, including the version number.
+     *
+     * @return Application name string.
+     */
+    public static String getNameFull() {
+        return APP_NAME + " v" + APP_VERSION_NAME;
+    }
+
     public static void main(String[] args) {
 	    // TODO: write your code here
     }
